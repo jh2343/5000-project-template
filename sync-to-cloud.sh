7f6da5dfec015e52b1d660c892ba3666d3c44e25
James Hickman <jameshickman@Jamess-MBP.lan>
2022-08-28 14:34:25 -0400

    GITHUB-UPLOAD:DATE-2022-08-28-TIME-14-34-25

--- sync-to-cloud.sh
+++ sync-to-cloud.sh
@@ -1,12 +1,9 @@
 
 
 # SYNC LOCAL VERSION OF WEBSITE TO GU-DOMAINS SERVER
-# rsync -alvr 
+rsync -alvr website jfhgeorg@gtown.reclaimhosting.com:/home/jfhgeorg/public_html/
 
 # PUSH GIT REPO TO THE CLOUD FOR BACKUP
-#DATE=$(date) #$(date +"%Y-%m-%d")
-#message="BACKING UP ANLY-501 PROJECT TO GITHUB SERVERS: "$DATE; 
-
 DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
 message="GITHUB-UPLOAD:$DATE";
 echo "commit message = "$message; 
